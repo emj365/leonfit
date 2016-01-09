0fc7d8ef9c373a77fe48774f91a6c4e60b296170
Michael JIN <emj365@gmail.com>
2016-01-09 18:29:49 +0800

    fix grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
           '<%= yeoman.app %>/**/*.{html,yml,md,mkd,markdown}',
           '!<%= yeoman.app %>/_bower_components/**/*'
         ],
-        tasks: ['bundle exec jekyll:server']
+        tasks: ['jekyll:server']
       }
     },
     browserSync: {
